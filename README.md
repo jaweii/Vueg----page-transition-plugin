9dff8fd59cbe8e887e9cee5f2b478e81111ce0e1
jawei <1005945485@qq.com>
2017-06-12 22:35:48 +0800

    完善配置项

--- README.md
+++ README.md
@@ -10,12 +10,12 @@ just need to add `v-transition` in the `<router-view v-transition></router-view>
 ----------
 
 
-##效果图 / Demo##：
+## 效果图 / Demo ##：
 
 ![演示动态图][1]
 
 ----------
-##使用方法 / Usage##
+## 使用方法 / Usage ##
 
 0、安装 / Installation
 
@@ -43,13 +43,22 @@ just need to add `v-transition` in the `<router-view v-transition></router-view>
     
 ----------
 ## 配置项 ##
-        const options={
-            duration: '0.3',//转场动画时长，默认0.3
-            firstEntryDuration: '.6',//首次进入渐进时长，默认0.6
-            forwardAnim:'fadeInRight',//前进动画，默认fadeInRight
-            backAnim:'fadeInLeft'//后退动画，默认fadeInLeft
-        }
+        const options={  
+            duration: '0.3', //转场动画时长  
+            firstEntryDisable: false, //值为true时禁用首次进入的渐进动画  
+            firstEntryDuration: '.6', //首次进入渐进动画时长  
+            forwardAnim: 'fadeInRight', //前进动画  
+            backAnim: 'fadeInLeft', //后退动画  
+            sameDepthdisable: false, //url级别相同时禁用动画  
+            tabs: [{
+                    name:'home'
+                },{
+                    name:'my'
+                }], //默认为[]，name对应路由的name,以实现类似app中点击tab页面水平转场效果，如tab[1]到tab[0]  ，会使用forwardAnim动画，tab[1]到tab[2]，会使用backAnim动画  
+            tabsDisable: false, //值为true时，tabs间的转场没有动画  
+        }  
         Vue.use(vueg, router,options)
+
 `forwardAnim` 、 `backAnim` 提供一下值：  
 bounce  
 flash  
@@ -58,7 +67,7 @@ rubberBand
 shake  
 headShake  
 swing  
-.....省略几十个...  
+....略...  
 slideInDown  
 slideInLeft  
 slideOutDown  
@@ -66,6 +75,17 @@ slideOutLeft
 slideOutRight  
 slideOutUp  
 预览效果，以及查看全部可用值，请访问：https://daneden.github.io/animate.css/
+
+
+options还可以在每个组件的data中配置，举例：  
+            data(){
+                return {
+                    vugConfig:{
+                        forwardAnim:'bounceInUp',//options所有配置可以写在这个对象里，会覆盖全局的配置
+                    }
+            }
+        }
+
 
   [1]: https://raw.githubusercontent.com/jaweii/vueg/master/image/GIF.gif
   [2]: https://raw.githubusercontent.com/jaweii/vueg/master/image/vueg.JPG
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 let transition = {};
 transition.install = (Vue, router, options = {}) => {
     let route, lastPath, transitionType, binding = {}
-        //默认配置
-    let op = {
-        duration: '0.3',//动画时长
-        firstEntryDuration: '.6',//首次进入渐进时长
-        forwardAnim:'fadeInRight',//前进动画
-        backAnim:'fadeInLeft'//后退动画
 
+    //默认配置
+    let op = {
+        duration: '0.3', //动画时长
+        firstEntryDisable: false, //值为true时禁用首次进入的渐进动画
+        firstEntryDuration: '.6', //首次进入渐进动画时长
+        forwardAnim: 'fadeInRight', //前进动画
+        backAnim: 'fadeInLeft', //后退动画
+        sameDepthdisable: false, //url级别相同时禁用动画
+        tabs: [], //name填写对应路由的name,以实现类似app中点击tab页面水平转场效果，如tab[1]到tab[0]，会使用forwardAnim动画，tab[1]到tab[2]，会使用backAnim动画
+        tabsDisable: false, //值为true时，tabs间的转场没有动画
+        disable: false, //禁用转场动画
     }
-    Object.keys(options).forEach(key => {
-        op[key] = options[key]
-    })
 
     Vue.directive('transition', {
         bind(el, _binding, vnode, oldVnode) {
@@ -34,20 +36,61 @@ transition.install = (Vue, router, options = {}) => {
         if (from.path.charAt(from.path.length - 1) !== '/')
             fromDepth += 1
         transitionType = toDepth > fromDepth ? 'forward' : 'back'
+
+        //深度相同
         if (toDepth === fromDepth) {
-            if (lastPath === to.path)
+            if (lastPath === to.path) {
                 transitionType = 'back'
-            else transitionType = 'forward'
+            } else {
+                transitionType = 'forward'
+            }
+            //深度相同时禁用动画
+            if (op.sameDepthdisable)
+                transitionType = ''
+
             lastPath = from.path
         }
-        if (to.path === from.path && to.path === lastPath) //首页无效果
+
+        //首次进入无效果
+        if (to.path === from.path && to.path === lastPath)
+            transitionType = 'first'
+
+        //关闭首次进入渐进动画
+        if (op.firstEntryDisable)
             transitionType = ''
+
+        //tabs转场控制
+        if (from.name && to.name) {
+            let fromIndex = op.tabs.findIndex(item => {
+                    return item.name === from.name
+                }),
+                toIndex = op.tabs.findIndex(item => {
+                    return item.name === to.name
+                })
+            if (!op.tabsDisable && fromIndex !== -1 && toIndex !== -1) {
+                //启用tabs控制
+                if (toIndex > fromIndex)
+                    transitionType = 'forward'
+                if (toIndex < fromIndex)
+                    transitionType = 'back'
+                if (toIndex === fromIndex)
+                    transition = ''
+            } else {
+                //tabs禁用动画
+                if (fromIndex !== -1 && toIndex !== -1)
+                    transitionType = ''
+            }
+        }
+        //禁用转场动画配置
+        if (op.disable)
+            transition = ''
+
         next()
     })
 
     function addEffect(vm = this) {
         if (!vm)
-            return console.error('无vm')
+            return
         let el = vm.$el
         if (binding.value === false)
             return
@@ -55,14 +98,31 @@ transition.install = (Vue, router, options = {}) => {
             return console.error('没有使用vue-router')
         if (!el.parentElement)
             return
+
+        //全局vueg配置
+        Object.keys(options).forEach(key => {
+            op[key] = options[key]
+        })
+
+        //组件vueg配置
+        let vugConfig = vm.$data.vugConfig
+        if (vugConfig) {
+            Object.keys(vugConfig).forEach(key => {
+                op[key] = vugConfig[key]
+            })
+        }
+
         el.parentElement.style.overflow = 'hidden'
-            //转场动画时长
-        el.style.animationDuration = op.duration + 's'
-            //设置首次进入的渐进显示时长
-        if (!transitionType) {
+
+        //设置首次进入的渐进显示时长
+        if (transitionType === 'first') {
             el.style.animationDuration = op.firstEntryDuration + 's'
             el.classList.add('fadeIn')
         }
+
+        //转场动画时长
+        if (transitionType)
+            el.style.animationDuration = op.duration + 's'
 
         el.classList.add('animated')
         switch (transitionType) {
@@ -79,6 +139,7 @@ transition.install = (Vue, router, options = {}) => {
         setTimeout(() => {
             el.classList.remove(op.forwardAnim)
             el.classList.remove(op.backAnim)
+            el.style.animationDuration = '0s'
         }, op.duration * 1000)
         setTimeout(() => {
             el.classList.remove('fadeIn')
