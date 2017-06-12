78e77ec873639dea4935792f2a8d0828b9e2be7b
谢秀岳 <xxy10062@gmail.com>
2017-06-12 08:50:11 +0800

    Create index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let transition = {};
-transition.install = (Vue, options = {}) => {
+transition.install = (Vue, options) => {
     let route, lastPath, transitionType
     let op = { duration: '0.3s' } //默认配置
     Object.keys(options).forEach(key => {
@@ -16,7 +16,6 @@ transition.install = (Vue, options = {}) => {
             if (!transitionType) {
                 return el.classList.add('vue-transition-first')
             }
-
             el.style.animationDuration = op.duration + 's'
             if (transitionType === 'forward') {
                 el.classList.add('vue-transition-in')
@@ -32,20 +31,36 @@ transition.install = (Vue, options = {}) => {
         },
         watch: {
             '$route' (val, oldVal) {
+				
                 // 转场前进/后退控制
-                let toDepth = val.path.split('/').length
-                let fromDepth = oldVal.path.split('/').length
-                if (val.path.charAt(val.path.length - 1) !== '/')
+                let toDepth = val.path.split('/').length,
+					fromDepth = oldVal.path.split('/').length,
+					homeLevel = false,
+					homeLevelKey = false,// 首页参与过度
+					levelKey = false // 同级开启切换
+                if (val.path.charAt(val.path.length - 1) !== '/'){
                     toDepth += 1
-                if (oldVal.path.charAt(oldVal.path.length - 1) !== '/')
+				}
+                if (oldVal.path.charAt(oldVal.path.length - 1) !== '/'){
                     fromDepth += 1
-                transitionType = toDepth > fromDepth ? 'forward' : 'back'
-                if (toDepth === fromDepth) {
-                    if (lastPath === val.path)
-                        transitionType = 'back'
-                    else transitionType = 'forward'
-                    lastPath = oldVal.path
-                }
+				}
+				
+				if(!homeLevelKey&&val.path.split('/').join('')==''||oldVal.path.split('/').join('')==''){
+					homeLevel = true;
+				}
+				// 同级不转场
+				if(!levelKey&&toDepth==fromDepth||homeLevel){
+					transitionType='';
+					this.$el.style.animationDuration = '0s'
+					return false;
+				}
+				transitionType = toDepth > fromDepth ? 'forward' : 'back'
+				if (toDepth === fromDepth) {
+					if (lastPath === val.path)
+						transitionType = 'back'
+					else transitionType = 'forward'
+					lastPath = oldVal.path
+				}
             }
         }
     })
